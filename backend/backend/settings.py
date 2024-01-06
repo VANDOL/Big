bcedbf8a042925ea90102ea1d6c805218c9ca14a
Hogyun <ghrbs5083@naver.com>
2024-01-06 20:34:38 +0900

    secretkey_change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5ik-rnb7oqdb0x2kdce=qz_s-ii@6m-b(cqes@#awl^qyt06ng"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
