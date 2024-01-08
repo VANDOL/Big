dc85b4ad20026bc559cee0bf042ad5beb0dad6fe
jae young <yongsann1234@naver.com>
2024-01-08 20:02:10 +0900

    글작성수정삭제가능(작성자구분x)

--- backend/bulletinboard/models.py
+++ backend/bulletinboard/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.conf import settings
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    file = models.FileField(upload_to='uploads/', null=True, blank=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        default=1
+    )
 
 class File(models.Model):
     post = models.ForeignKey(Post, related_name='files', on_delete=models.CASCADE)
