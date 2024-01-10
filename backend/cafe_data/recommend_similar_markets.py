5a92a7e07187d97c241c235ab09a09e78ed21381
Lee Gyeong Min <103250300+Lee-GyeongMin95@users.noreply.github.com>
2024-01-10 09:41:03 +0900

    Update recommend_similar_markets.py

--- backend/cafe_data/recommend_similar_markets.py
+++ backend/cafe_data/recommend_similar_markets.py
@@ -94,16 +94,19 @@ def recommend_similar_markets(service_name, locations, user_preferences, n=5):
                 'Male','Female',
                 'Age_Group_20s','Age_Group_30s', 'Age_Group_40s', 'Age_Group_50s', 'Age_Group_60s']
     data_matrix = fuzzy_df3[features].values.T
+
+    num_clusters = 4
+    
 # =====
     scaler = MinMaxScaler()
     data_matrix = scaler.fit_transform(data_matrix)
 
-    init_kmeans = KMeans(n_clusters=c_value, init='k-means++')
+    init_kmeans = KMeans(n_clusters=num_clusters, init='k-means++')
     init_kmeans.fit(data_matrix)
 
     init_centers = init_kmeans.cluster_centers_
  # =====
-    num_clusters = 5
+    
     cntr, u, _, _, _, _, _ = fuzz.cluster.cmeans(
         data_matrix,
         num_clusters,
